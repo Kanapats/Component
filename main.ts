f515f53c752c882195239dcdff6be903d8d27f19
Kanapat Appamatta <140145377+Kanapats@users.noreply.github.com>
2023-08-01 21:51:24 +0700

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,26 +1,24 @@
 radio.onReceivedNumber(function (receivedNumber) {
     if (receivedNumber == 1) {
+        strip.showColor(neopixel.colors(NeoPixelColors.Red))
         makerbit.playMp3TrackFromFolder(1, 1, Mp3Repeat.No)
         basic.showIcon(IconNames.No)
     } else {
         makerbit.runMp3Command(Mp3Command.STOP)
         basic.showIcon(IconNames.SmallSquare)
+        strip.showRainbow(1, 360)
+        basic.pause(2000)
+        strip.showColor(neopixel.colors(NeoPixelColors.Green))
+        basic.pause(2000)
+        strip.showColor(neopixel.colors(NeoPixelColors.Blue))
+        basic.pause(2000)
+        strip.showColor(neopixel.colors(NeoPixelColors.Purple))
+        basic.pause(2000)
     }
 })
+let strip: neopixel.Strip = null
 radio.setGroup(38)
 makerbit.connectSerialMp3(DigitalPin.P2, DigitalPin.P1)
 makerbit.setMp3Volume(50)
-let strip = neopixel.create(DigitalPin.P16, 12, NeoPixelMode.RGB)
+strip = neopixel.create(DigitalPin.P16, 12, NeoPixelMode.RGB)
 basic.showIcon(IconNames.Yes)
-basic.forever(function () {
-    strip.showRainbow(1, 360)
-    basic.pause(2000)
-    strip.showColor(neopixel.colors(NeoPixelColors.Red))
-    basic.pause(2000)
-    strip.showColor(neopixel.colors(NeoPixelColors.Green))
-    basic.pause(2000)
-    strip.showColor(neopixel.colors(NeoPixelColors.Blue))
-    basic.pause(2000)
-    strip.showColor(neopixel.colors(NeoPixelColors.Purple))
-    basic.pause(2000)
-})
