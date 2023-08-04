1b322c70b0353c4a998c2f2879230ae6bcba8869
Kanapat Appamatta <140145377+Kanapats@users.noreply.github.com>
2023-08-04 08:09:09 +0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,23 +1,17 @@
-radio.onReceivedNumber(function (receivedNumber) {
-    if (receivedNumber == 1) {
+radio.onReceivedString(function (receivedString) {
+    if (receivedString == "Danger") {
         strip.showColor(neopixel.colors(NeoPixelColors.Red))
         makerbit.playMp3TrackFromFolder(1, 1, Mp3Repeat.No)
         basic.showIcon(IconNames.No)
+        basic.pause(3100)
     } else {
+        strip.showColor(neopixel.colors(NeoPixelColors.Green))
         makerbit.runMp3Command(Mp3Command.STOP)
         basic.showIcon(IconNames.SmallSquare)
-        strip.showRainbow(1, 360)
-        basic.pause(2000)
-        strip.showColor(neopixel.colors(NeoPixelColors.Green))
-        basic.pause(2000)
-        strip.showColor(neopixel.colors(NeoPixelColors.Blue))
-        basic.pause(2000)
-        strip.showColor(neopixel.colors(NeoPixelColors.Purple))
-        basic.pause(2000)
     }
 })
 let strip: neopixel.Strip = null
-radio.setGroup(38)
+radio.setGroup(138)
 makerbit.connectSerialMp3(DigitalPin.P2, DigitalPin.P1)
 makerbit.setMp3Volume(50)
 strip = neopixel.create(DigitalPin.P16, 12, NeoPixelMode.RGB)
